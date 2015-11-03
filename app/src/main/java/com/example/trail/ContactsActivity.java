7f3da1f9108cddffbacdd839def6cd9abb877482
Shrabanti1995 <shrabanti13096@iiitd.ac.in>
2015-11-04 03:23:25 +0530

    Successful changes My layouts modified ContactsActivity Added and correctly working

---
+++ app/src/main/java/com/example/trail/ContactsActivity.java
@@ -0,0 +1,115 @@
+package com.example.trail;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    private ArrayList<String> nameOfContacts = new ArrayList<String>();
+    private ArrayList<String> phoneNoOfContacts = new ArrayList<String>();
+    private ActionMode mActionMode;
+    private ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_contacts);
+
+        fetchContacts(this.getContentResolver());
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(ContactsActivity.this, PrivatechatActivity.class);
+                startActivity(i);
+            }
+        });
+
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //return false;
+                //Toast.makeText(ContactsActivity.this,"Wanna make a group?",Toast.LENGTH_SHORT).show();
+                mActionMode = startActionMode(new ActionModeCallback());
+                return true;
+            }
+        });
+    }
+
+    public void fetchContacts(ContentResolver cr)
+    {
+        Cursor phones = cr.query(ContactsContract.Contacts.CONTENT_URI, null,null,null, null);
+        if(phones.getCount() > 0)
+        {
+            while (phones.moveToNext())
+            {
+                String contactId = phones.getString(phones.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = phones.getString(phones.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                if (Integer.parseInt(phones.getString(phones.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0)
+                {
+                    //Query and loop for every phone number of the contact
+                    Cursor phoneCursor = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[] {contactId}, null);
+                    if(phoneCursor.moveToNext())
+                    {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        nameOfContacts.add(name);
+                        phoneNoOfContacts.add(phoneNumber);
+                    }
+                    phoneCursor.close();
+                }
+            }
+        }
+        phones.close();
+
+        CustomPhoneListAdapter adapter=new CustomPhoneListAdapter(this, nameOfContacts, phoneNoOfContacts);
+        list=(ListView)findViewById(R.id.listView2);
+        list.setAdapter(adapter);
+    }
+
+    private class ActionModeCallback implements ActionMode.Callback {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // inflate contextual menu
+            mode.getMenuInflater().inflate(R.menu.contextual_contacts_list_view, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            Toast.makeText(ContactsActivity.this, "Wanna create a group?", Toast.LENGTH_LONG).show();
+
+            //PERFORM TASKS ON THE CLICK OF CREATE GROUP
+
+
+            // close action mode
+            mode.finish();
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            // remove selection
+            mActionMode = null;
+        }
+    }
+}
